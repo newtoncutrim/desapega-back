a19dcb66c9b7e18ac270a305e53e7cee8e034229
newtoncutrim <newtoncutrim777@gmail.com>
2023-07-20 11:36:16 -0300

    protecao de crf token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,8 +9,31 @@ use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
-    public function index(Request $r, User $user): JsonResponse {
+    public function index(Request $r, User $user): JsonResponse
+    {
         $data = $user->all();
         return response()->json(['data' => $data]);
+    }
+
+    public function signup(): JsonResponse
+    {
+        return response()->json([
+            'method' => 'signup'
+        ]);
+
+    }
+    public function signin(): JsonResponse
+    {
+        return response()->json([
+            'method' => 'signin'
+        ]);
+
+    }
+    public function data(): JsonResponse
+    {
+        return response()->json([
+            'method' => 'data'
+        ]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\StatesController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/ping', function(): JsonResponse {
     return response()->json(['pong' => true]);
 });
 
 Route::get('/categories', [CategoriesController::class, 'index']);
-
 Route::get('/states', [StatesController::class, 'index']);
-
 Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/user/signup', [UserController::class, 'signup']);
+Route::post('/user/signin', [UserController::class, 'signin']);
+Route::post('/user/data', [UserController::class, 'data'])->withoutMiddleware(VerifyCsrfToken::class);
+/* metodo de exemplo crf token */
